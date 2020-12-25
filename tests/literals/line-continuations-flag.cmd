0d6b70adf3445bc3aa5114186bc37197aaa22eb4
Conway <yawnoc@pm.me>
2020-12-26 01:29:32 +0800

    Add CMD literals line continuations flag test

---
+++ tests/literals/line-continuations-flag.cmd
@@ -0,0 +1,16 @@
+~~
+Text\
+{line continuation not processed}
+~~
+c~~
+Text\
+{line continuation processed}
+Text\
+  {2 spaces gobbled}
+Text\
+		{2 tabs gobbled}
+Text\
+ 	{1 space, 1 tab gobbled}
+Text \
+  {1 space before line continuation}
+~~
